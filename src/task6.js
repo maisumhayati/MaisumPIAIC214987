06dcd1b8d3baebfe9b3f912cef35efdcfb84a080
Maisum Hayati <hayatimaisum@gmail.com>
2023-07-08 19:20:31 +0500

    Task 6 completed

---
+++ src/task6.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Stripping Names: Store a person’s name,
+and include some whitespace characters at the beginning and end of the name.
+Make sure you use each character combination, "\t" and "\n", at least once.
+Print the name once, so the whitespace around the name is displayed.
+Then print the name after striping the white spaces. */
+Object.defineProperty(exports, "__esModule", { value: true });
+var personName = "\tMaisum\t \nHayati\n";
+console.log("With Whitespaces:\n ".concat(personName));
+console.log("Without Whitespaces:\n ".concat(personName.replace(/\s+/g, " ")));
