34a6f6290e881b5a7adc3cdbf9dc7f1b20588036
Maisum Hayati <hayatimaisum@gmail.com>
2023-07-08 18:21:32 +0500

    Task 4 completed

---
+++ src/Task4.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Famous Quote: Find a quote from a famous person you admire.
+Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
+Object.defineProperty(exports, "__esModule", { value: true });
+var famousPerson = "Harvey Specter";
+var quote = "When you’re backed against the wall, break the goddamn thing down.";
+console.log("".concat(famousPerson, " once said, ").concat(quote));
