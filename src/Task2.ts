2e563f4a8c897536a71cd633defddbdf5cc2a5a0
Maisum Hayati <hayatimaisum@gmail.com>
2023-07-08 17:39:55 +0500

    src folder created

---
+++ src/Task2.ts
@@ -0,0 +1,9 @@
+/*Personal Message: Store a person’s name in a variable, 
+and print a message to that person. 
+Your message should be simple, such as,
+“Hello Eric, would you like to learn some Python today?”*/
+
+let personName: string = "Maisum Hayati";
+let messageToPerson: string = `Hello ${personName}, are you excited to do some typescripting :D`;
+
+console.log(messageToPerson);
