fb6f312789b89f025ef946d6a83146fa60f11501
Maisum Hayati <hayatimaisum@gmail.com>
2023-07-10 00:28:20 +0500

    Task7 completed

---
+++ src/Task7.ts
@@ -0,0 +1,13 @@
+/* Number Eight: Write addition, subtraction, multiplication,
+ and division operations that 
+ each result in the number 8. 
+ Be sure to enclose your operations in print statements to see the results. */
+
+import { sum, subtract, divide, multiply } from "mathjs";
+
+console.log(sum(5, 3)); //addition
+console.log(subtract(11, 3)); //subtraction
+console.log(divide(16, 2)); //division
+console.log(multiply(4, 2)); //multiplication
+
+export {};
---
+++ src/task7.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Number Eight: Write addition, subtraction, multiplication,
+ and division operations that
+ each result in the number 8.
+ Be sure to enclose your operations in print statements to see the results. */
+Object.defineProperty(exports, "__esModule", { value: true });
+var mathjs_1 = require("mathjs");
+console.log((0, mathjs_1.sum)(5, 3)); //addition
+console.log((0, mathjs_1.subtract)(11, 3)); //subtraction
+console.log((0, mathjs_1.divide)(16, 2)); //division
+console.log((0, mathjs_1.multiply)(4, 2)); //multiplication
