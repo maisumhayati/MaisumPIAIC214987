032bced047fc2e09fd4759c56be98456ed526b83
Maisum Hayati <hayatimaisum@gmail.com>
2023-07-10 00:45:50 +0500

    Task 8 completed

---
+++ src/Task8.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Favorite Number: Store your favorite number in a variable.
+Then, using that variable,
+create a message that reveals your favorite number. Print that message. */
+Object.defineProperty(exports, "__esModule", { value: true });
+var favouriteNum = 4;
+console.log("My favourite number is: ".concat(favouriteNum));
---
+++ src/Task8.ts
@@ -0,0 +1,9 @@
+/* Favorite Number: Store your favorite number in a variable. 
+Then, using that variable, 
+create a message that reveals your favorite number. Print that message. */
+
+let favouriteNum = 4;
+
+console.log(`My favourite number is: ${favouriteNum}`)
+
+export{};
